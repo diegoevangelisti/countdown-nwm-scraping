086a45a93c383535d2a0d151617a04f82787d25a
Julio Truzzi <julio.truzzi@gmail.com>
2019-03-04 02:42:39 -0300

    working version

--- app.js
+++ app.js
@@ -1,110 +1,132 @@
-//STEP 2
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const chalk = require("chalk");
+const _ = require("lodash");
 
-//Last scraping in categoty_id = 7
-//Date 01/03
+var categories = require("./categories.json");
 
-//Get all products
+const processCategories = async () => {
+  categories.forEach(async category => {
+    let pageCounter = 0;
+    let pageLimit = 1;
+    const parsedProducts = [];
 
-// External dependencies
-const axios = require('axios')
-const cheerio = require('cheerio')
-const fs = require('fs')
-const chalk = require('chalk')
+    const url = category.url + "?page=" + pageCounter;
+    const response_p = await axios.get(url);
 
-const outputFile_p = 'products_7.json'
-const parsedProducts = []
+    const $ = cheerio.load(response_p.data);
+    pageLimit = $("ul.paging.pull-left.hidden-phone")
+      .find("li.page-number")
+      .last()
+      .find("a._jumpTop")
+      .text()
+      .trim();
 
-var pageCounter = 0
-var pageLimit = 1
-var cat_data = require('./categories.json');
-var categoryNumber = cat_data.length;
-let IDCount = 3267;
-var i = 7;
-
-//Number of categories
-console.log(chalk.yellow.bgBlue(`\n Number of categories obtained from file: ${categoryNumber}\n`))
-
-//Get url to scrape
-var pageurl = cat_data[i].url
-
-console.log(chalk.yellow.bgBlue(`\n  Scraping of ${chalk.underline.bold(pageurl)} initiated...\n`))
-
-async function getWebsiteContent_p(pageurl) {
     try {
-        const response_p = await axios.get(pageurl)
-        const $ = cheerio.load(response_p.data)
+      const pages = await Promise.all(
+        [...Array(parseInt(pageLimit - 1))].map(async (item, i) => {
+          const pageUrl = category.url + "?page=" + i;
+          try {
+            const response = await axios.get(pageUrl);
+            console.log(
+              `Processing ${category.category_name}: ${i} out of ${pageLimit}`
+            );
 
-        if (pageCounter == 0) {
-            pageLimit = $('ul.paging.pull-left.hidden-phone').find('li.page-number').last().find('a._jumpTop').text().trim()
-            Number(pageLimit)
-            console.log(chalk.yellow.bgBlue(`\n Number of pages: ${pageLimit}\n`))
-        }
-
-        //Scraping products
-        $("div.gridProductStamp-product").map((i, el) => {
-            count = IDCount++
-
-            var offer_p = offer_price = null;
-            let unit = null;
-
-            const name = $(el).find("h3.gridProductStamp-name").text().trim()
-            var normal_p = $(el).find("div.gridProductStamp-price.din-medium").text().trim()
-
-            if (normal_p != "") {
-                normal_price = (normal_p.replace(/\D+/g, '') / 100).toFixed(2);
-
-            } else {
-                if (normal_p == "") {
-
-                    offer_p = $(el).find("span.gridProductStamp-price.savings-text.din-medium").text().trim()
-                    normal_p = $(el).find("span.gridProductStamp-subPrice").text().trim()
-                    offer_price = (offer_p.replace(/\D+/g, '') / 100).toFixed(2);
-                    normal_price = (normal_p.replace(/\D+/g, '') / 100).toFixed(2);
-                }
-            }
-            const product = {
-                product_id: count,
-                category_id: cat_data[i]._id,
-                product_name: name,
-                product_price: {
-                    normal_price,
-                    offer_price
-                },
-                measure_unit: unit,
-                url: pageurl
-            }
-            parsedProducts.push(product)
+            return parsePage(response, category, pageUrl);
+          } catch (e) {
+            console.info(e);
+            return null;
+          }
         })
-
-        pageCounter++
-
-        if (pageCounter < pageLimit) {
-
-            //Get next page link to continue scraping in the same category
-            console.log(chalk.cyan(`  Scraping: ${pageurl}`))
-            const nextPageLink = 'https://shop.countdown.co.nz' + $('ul.paging.pull-left.hidden-phone').children('li.next').find('a').attr('href')
-            getWebsiteContent_p(nextPageLink)
-
-        } else {
-            //No more pages in the category
-            console.log(chalk.cyan(`  Scraping: ${pageurl}`))
-            exportResults(parsedProducts)
-            pageCounter = 0;
-            return false
-        }
-
+      );
+      exportResults(category, pages);
     } catch (error) {
-        exportResults(parsedProducts)
-        console.error(error)
+      console.info(error);
     }
+  });
+};
+
+function parsePage(response, category, pageUrl) {
+  //Scraping products
+  const $ = cheerio.load(response.data);
+
+  return $("div.gridProductStamp-product")
+    .map((i, el) => {
+      var offer_p = (offer_price = null);
+      let unit = null;
+
+      const name = $(el)
+        .find("h3.gridProductStamp-name")
+        .text()
+        .trim();
+      var normal_p = $(el)
+        .find("div.gridProductStamp-price.din-medium")
+        .text()
+        .trim();
+
+      if (normal_p != "") {
+        normal_price = (normal_p.replace(/\D+/g, "") / 100).toFixed(2);
+      } else {
+        if (normal_p == "") {
+          offer_p = $(el)
+            .find("span.gridProductStamp-price.savings-text.din-medium")
+            .text()
+            .trim();
+          normal_p = $(el)
+            .find("span.gridProductStamp-subPrice")
+            .text()
+            .trim();
+          offer_price = (offer_p.replace(/\D+/g, "") / 100).toFixed(2);
+          normal_price = (normal_p.replace(/\D+/g, "") / 100).toFixed(2);
+        }
+      }
+      return new Product(
+        category.category_id,
+        name,
+        normal_price,
+        offer_price,
+        unit,
+        pageUrl
+      );
+    })
+    .get();
 }
 
-const exportResults = (parsedProducts) => {
-fs.writeFile(outputFile_p, JSON.stringify(parsedProducts, null, 4), (err) => {
-    if (err) {
-        console.log(err)
-    }
-    console.log(chalk.yellow.bgBlue(`\n ${chalk.underline.bold(parsedProducts.length)} Results exported successfully to ${chalk.underline.bold(outputFile_p)}\n`))
-})
+const exportResults = (category, pages) => {
+  const fileName = `categories/${category.category_name}.json`;
+  console.info(`Saving ${fileName}`);
+
+  // We flatten the array because we ended up with an array of arrays
+  fs.writeFile(fileName, JSON.stringify(_.flatten(pages), null, 4), err => {});
+};
+
+class Product {
+  constructor(
+    product_id,
+    category_id,
+    product_name,
+    normal_price,
+    offer_price,
+    measure_unit,
+    url
+  ) {
+    this.product_id = Math.random()
+      .toString(36)
+      .substr(2, 9);
+    this.category_id = category_id;
+    this.product_name = product_name;
+    this.product_price = new ProductPrice(normal_price, offer_price);
+    this.measure_unit = measure_unit;
+    this.url = url;
+  }
 }
-getWebsiteContent_p(pageurl)
+
+class ProductPrice {
+  constructor(normal_price, offer_price) {
+    this.normal_price = normal_price;
+    this.offer_price = offer_price;
+  }
+}
+
+processCategories();
