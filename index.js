729e7da334acdf7a02d65c2da03c8b83588d4224
Diego <evangelistidiego@gmail.com>
2019-08-06 12:14:48 +1200

    domain updated

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.use((error, req, res, next) => {
 });
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
